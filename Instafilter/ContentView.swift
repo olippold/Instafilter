fdb03c57325a114f940898e816528bf8fc07ae3f
Oliver Lippold <olippold@hotmail.com>
2020-02-02 14:09:27 +0000

    Completed Day 62

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingActionSheet = false
+    @State private var backgroundColour = Color.white
+    
     var body: some View {
         Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColour)
+            .onTapGesture {
+                self.showingActionSheet = true
+        }
+        .actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("Change background"), message: Text("Select a new colour"), buttons: [
+                .default(Text("Red")) { self.backgroundColour = .red },
+                .default(Text("Green")) { self.backgroundColour = .green },
+                .default(Text("Blue")) { self.backgroundColour = .blue },
+                .cancel()
+            ])
+        }
     }
 }
 
