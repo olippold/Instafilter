eb3fc04d652dce438006b17c9818cc4762f191c5
Oliver Lippold <olippold@hotmail.com>
2020-02-15 14:07:47 +0000

    Completed day 66

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -15,18 +15,21 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter:  CIFilter = CIFilter.sepiaTone()
+    @State private var showingFilterSheet = false
+    @State private var processedImage: UIImage?
+    
     let context = CIContext()
     
     var body: some View {
         let intensity = Binding<Double>(
             get: {
                 self.filterIntensity
-            },
+        },
             set: {
                 self.filterIntensity = $0
                 self.applyProcessing()
-            }
+        }
         )
         
         return NavigationView {
@@ -57,13 +60,22 @@ struct ContentView: View {
                 
                 HStack {
                     Button("Change Filter") {
-                        // change filter
+                        self.showingFilterSheet = true
                     }
                     
                     Spacer()
                     
                     Button("Save") {
-                        // save the picture
+                        guard let processedImage = self.processedImage else { return }
+                        
+                        let imageSaver = ImageSaver()
+                        imageSaver.successHandler = {
+                            print("Success")
+                        }
+                        imageSaver.errorHandler = {
+                            print("Oops: \($0.localizedDescription)")
+                        }
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
                     }
                 }
             }
@@ -71,6 +83,19 @@ struct ContentView: View {
             .navigationBarTitle("InstaFilter")
             .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
                 ImagePicker(image: self.$inputImage)
+                
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystallise")) { self.setFilter(CIFilter.crystallize())},
+                    .default(Text("Edges")) { self.setFilter(CIFilter.edges())},
+                    .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur())},
+                    .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate())},
+                    .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone())},
+                    .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask())},
+                    .default(Text("Vignette")) { self.setFilter(CIFilter.vignette())},
+                    .cancel()
+                ])
             }
         }
     }
@@ -84,13 +109,22 @@ struct ContentView: View {
     }
     
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) {currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)}
         guard let outputImage = currentFilter.outputImage else { return }
         
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 }
 
---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Oliver Lippold on 15/02/2020.
+//  Copyright © 2020 Oliver Lippold. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
